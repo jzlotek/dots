231a0637dbcf2f38fbf3b25c573ca0e7bfbb71a8
John Zlotek <jzlotek@gmail.com>
2020-02-03 15:59:26 -0500

    fix spacing on vimrc

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 echo 'alias dots="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
 source ~/.zshrc
+
+alias dots="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+
 echo ".dotfiles.git" >> .gitignore
 git clone --bare git@github.com:jzlotek/dots.git $HOME/.dotfiles.git
 dots checkout
