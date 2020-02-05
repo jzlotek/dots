ef3f9b739c9b1c6628c92ebdf1f3a2f6d50e334c
John Zlotek <jzlotek@gmail.com>
2020-02-05 16:00:13 -0500

    Thinkpad Update

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
+# THIS WILL OVERWRITE ALL YOUR DOTFILES
+# USE AT YOUR OWN RISK
 
 echo 'alias dots="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
 source ~/.zshrc
 
-alias dots="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+dots="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 
 echo ".dotfiles.git" >> .gitignore
 git clone --bare git@github.com:jzlotek/dots.git $HOME/.dotfiles.git
-dots checkout
-dots config --local status.showUntrackedFiles no
+
+# overwriting all dotfiles
+$dots checkout -f
+$dots config --local status.showUntrackedFiles no
