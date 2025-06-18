4402128f19c00dc2c13479ff39eede7039e3795f
mjheart <mjheart97@protonmail.com>
2025-06-18 10:13:41 -0400

    fix typo in method call

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -365,7 +365,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end, { desc = 'Format current buffer with LSP' })
 
     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+    if client and client:supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
       local highlight_augroup = vim.api.nvim_create_augroup('main-lsp-highlight', { clear = false })
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
         buffer = event.buf,
